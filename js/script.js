171853bdb0ffc7016cf4e2a1e8ff7f461792b49c
SophiaA <sophia.antonellis@gmail.com>
2021-08-11 18:06:41 -0400

    added API key to js file along with fetch function

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let weather = {
+    "apiKey": "5a1bf8388be4d30bb8e08bbbe2e40c87"
+fetchWeather: function () {
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
+}
+}
