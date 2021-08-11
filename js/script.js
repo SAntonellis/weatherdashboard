f3036e7f753d9e88bced67a5d95c74fb105b7853
SophiaA <sophia.antonellis@gmail.com>
2021-08-11 18:16:59 -0400

    corrected path to js file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let weather = {
-    "apiKey": "5a1bf8388be4d30bb8e08bbbe2e40c87"
+    "apiKey": "5a1bf8388be4d30bb8e08bbbe2e40c87",
 fetchWeather: function () {
-    fetch ("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
+    .then((data) => console.log(data));
 }
 }
+
+weather.fetchWeather()
