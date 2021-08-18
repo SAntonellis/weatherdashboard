2d38ece91c889ca642d81f3277f22ca11758b1d4
SophiaA <sophia.antonellis@gmail.com>
2021-08-18 17:07:15 -0400

    got api key working

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let weather = {
-    "apiKey": "5a1bf8388be4d30bb8e08bbbe2e40c87",
-fetchWeather: function () {
-    fetch ("https://api.openweathermap.org/data/2.5/weather?q=" 
-    + city 
-    + "&units=imperial&appid="
-    + appKey; 1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
-    .then((data) => console.log(data));
-},
-displayWeather: function(data) {
-    
-}
+    apiKey: "5a1bf8388be4d30bb8e08bbbe2e40c87",
+fetchWeather: function (city) {
+        fetch ("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid="
+        + this.apiKey 
+    )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+        
+    }
 };
 
 
 
-weather.fetchWeather()
+weather.fetchWeather("denver")
