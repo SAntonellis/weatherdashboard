6c182496803a0114777d4d9881b3b7b577733cc4
SophiaA <sophia.antonellis@gmail.com>
2021-08-18 16:58:35 -0400

    updating after save

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 let weather = {
     "apiKey": "5a1bf8388be4d30bb8e08bbbe2e40c87",
 fetchWeather: function () {
-    fetch ("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=" 
+    + city 
+    + "&units=imperial&appid="
+    + appKey; 1d7834557dcbeac19bc089229fef749e").then((response) => response.json())
     .then((data) => console.log(data));
+},
+displayWeather: function(data) {
+    
 }
-}
+};
+
+
 
 weather.fetchWeather()
