46e304c8df5e27207f2fe285443a408c1574be7b
SophiaA <sophia.antonellis@gmail.com>
2021-08-18 18:18:30 -0400

    updated rest of api variables and linked to html elements

--- js/script.js
+++ js/script.js
@@ -7,13 +7,25 @@ fetchWeather: function (city) {
         + this.apiKey 
     )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
         const { name } = data;
+        const { country } = data.sys;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name + ",";
+        document.querySelector(".country").innerText = country;
+        document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = "Current conditions: " + description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " m/ph";
     }
 };
 
 
 
-weather.fetchWeather("denver")
+weather.fetchWeather("wellington")
